859900d99e84a9aad386f8cde54eb9835c905135
ian <ianvrijs@icloud.com>
2023-03-30 10:37:22 +0200

    loginv2

--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
 from flask import *
 import sqlite3 as sql
+from db import *
 
 app = Flask(__name__)
 app.secret_key = 'ftygfuhijo123498joif1~!'
 
 @app.route('/')
-def index(): 
+def index():
     return render_template('index.html')
 
 @app.route('/logout') 
@@ -17,11 +18,10 @@ def logout():
 @app.route('/login', methods=['GET','POST'])
 def login():
     """Checkt of een gebruiker een actieve login sessie heeft. Nee? Laat login pagina zien. Ja? Laat index zien"""
-
-    #maak db connectie + cursor (vervang later door db class)
-    con = sql.connect('ACS.db')
+    #maak db connectie
+    con = Database().get_connection()
     con.row_factory = sql.Row
-    cur = con.cursor()
+    cur = Database().get_cursor(con)
     if 'loggedin' in session.keys() and session['loggedin']: #omslachtige dubbele check om een undefined keys foutmelding te voorkomen
         return redirect('/') #als er al een actieve login sessie is -> ga naar index
     else:
@@ -30,12 +30,13 @@ def login():
             password = request.form.get('password')
 
             cur.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+            #kan hier geen db class gebruiken: meerdere parameters
             row = cur.fetchone()
 
             if row: #als een account matcht aan de logingegevens
                 session['loggedin'] = True
                 session['username'] = row['username']
-                session['id'] = row['id']
+                session['id'] = row['rowid']
                 return render_template('index.html', message='Succesvol ingelogt!\n')#index + succes melding na geslaagde inlogpoging
             session['loggedin'] = False
             return render_template('login.html', error = 'Incorrecte inloggegevens. \n')#standaard render + foutmelding na foutieve inlogpoging
