9aa4f2afd6a65cff2de68a77b52765f44fbbff22
HMoussa98 <wordpresstest1.0.0@gmail.com>
2023-04-19 19:38:31 +0200

    Dashboard frontend

--- server/app.py
+++ server/app.py
@@ -1,20 +1,87 @@
-from flask import Flask, render_template, flash, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Email
+from dotenv import load_dotenv
+import os
+from flask import *
+import sqlite3 as sql
+from db import *
+# import requests
 
+# Used to get environment variables
+load_dotenv()
+
+# Flask app setup
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "jhdjhadgawWFajhfbyf367212DEy"
+app.secret_key = os.getenv("SECRET_KEY")
 
-class Login(FlaskForm):
-    email = StringField(render_kw={"placeholder": "E-mail"}, validators=[Email()])
-    password = PasswordField(render_kw={"placeholder": "Wachtwoord"})
-    submit = SubmitField("Login")
-
+##### Login page #####
+# Render home/login page
 @app.route('/')
-def login():
-    form = Login()
-    return render_template('login.html', form=form)
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
+# Logout
+@app.route('/logout') 
+def logout():
+    """cleart de sessie van de gebruiker, redirect naar index"""
+    session.clear()
+    return redirect('/')
+
+# Login
+@app.route('/login', methods=['GET','POST'])
+def login():
+    """Checkt of een gebruiker een actieve login sessie heeft. Nee? Laat login pagina zien. Ja? Laat index zien"""
+    #maak db connectie
+    con = Database().get_connection()
+    cur = Database().get_cursor(con)
+    if 'loggedin' in session.keys() and session['loggedin']: #omslachtige dubbele check om een undefined keys foutmelding te voorkomen
+        return redirect('/') #als er al een actieve login sessie is -> ga naar index
+    else:
+        if request.method == 'POST': #als er een inlog request is gedaan
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            cur.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+            #kan hier geen db class gebruiken: meerdere parameters
+            row = cur.fetchone()
+
+            if row: #als een account matcht aan de logingegevens
+                session['loggedin'] = True
+                session['username'] = row['username']
+                session['id'] = row['rowid']
+                return redirect('dashboard')#index + succes melding na geslaagde inlogpoging
+            session['loggedin'] = False
+            return render_template('login.html', error = 'Incorrecte inloggegevens. \n')#standaard render + foutmelding na foutieve inlogpoging
+        return(render_template('login.html'))#standaard render voor post
+
+
+##### Dashboard page #####
+# Render dashboard
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+##### Sensor data #####
+@app.route('/mlxData', methods=['GET', 'POST'])
+def get_mlx_data():
+    """Receive and send data of the MLX90640 camera sensor"""
+    if request.method == 'GET':
+        # Send MLX data when requested by the dashboard
+        pass
+    if request.method == 'POST':
+        # Receive data from the MLX sensor and store it in the database
+        pass
+
+@app.route('/shtData', methods=['GET', 'POST'])
+def get_sht_data():
+    """Receive and send data of the SHT21 sensor"""
+    if request.method == 'GET':
+        # Send SHT data when requested by the dashboard
+        pass
+    if request.method == 'POST':
+        # Receive data from the SHT sensor and store it in the database
+        pass
+
+
+##### Main #####
+if __name__ == '__main__':
     app.run(debug=True)
