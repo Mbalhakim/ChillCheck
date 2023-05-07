564af3b340808917c9dbe4c20347503c96f40696
devranaydinoglu <d.s.aydinoglu@gmail.com>
2023-05-07 15:52:23 +0200

    Added find, create, and delete functionality for the MLX90640 data table

--- server/models.py
+++ server/models.py
@@ -1,12 +1,26 @@
+from db import Database
 
 class User():
     def __init__():
         pass
 
-class MlxSensor():
-    def __init__():
-        pass
+class MlxData():
+    def __init__(self, id_ = 0, min_temp = 0, max_temp = 0, avg_temp = 0, created_at = ""):
+        self.id_ = id_
+        self.min_temp = min_temp
+        self.max_temp = max_temp
+        self.avg_temp = avg_temp
+        self.created_at = created_at
+    
+    def find(self, column, value):
+        return Database().find("MlxData", column, value)
 
-class ShtSensor():
+    def create(self):
+        Database().create("MlxData", (self.min_temp, self.max_temp, self.avg_temp))
+    
+    def delete_by_id(self, id_):
+        Database().delete_by_id("MlxData", id_)
+
+class ShtData():
     def __init__():
         pass
