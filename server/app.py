b26a5cec6cc5579d5f52b5f35e6017f2a2a4db70
HMoussa98 <wordpresstest1.0.0@gmail.com>
2023-06-08 21:21:41 +0200

    register + login

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from db import *
 from models import *
 import db
 import bcrypt
-
+import hashlib
 
 import requests
 import json
@@ -23,7 +23,8 @@ app.secret_key = os.getenv("SECRET_KEY")
 # Render home/login page
 @app.route('/')
 def index():
-    return render_template('dashboard.html')
+    
+    return render_template('login.html')
 
 # Logout
 @app.route('/logout') 
@@ -35,6 +36,17 @@ def logout():
 # Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if 'user_id' in session:
+        return redirect('dashboard')
+    
+    conn = sqlite3.connect("acs.db")
+    cursor = conn.cursor()
+    
+    def hash_password(password):
+        # Hash the password using SHA-256
+        hash_object = hashlib.sha256(password.encode())
+        return hash_object.hexdigest()
+
     if request.method == 'POST':
         first_name = request.form.get('first_name')
         last_name = request.form.get('last_name')
@@ -43,38 +55,62 @@ def register():
         company = request.form.get('company')
         birth_date = request.form.get('birth_date')
         role = request.form.get('role')
+        account_type = 0
+        created_at = date.today()
 
-        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-
-        user = User(first_name=first_name, last_name=last_name, username=email, hashed_password=hashed_pw, 
-                    company=company, date_of_birth=birth_date, role=role)
-        created = user.create()
-
-        if created:
-            flash(created)
+        hashed_pw = hash_password(password)
+        
+        cursor.execute("SELECT * FROM User WHERE email=?", (email,))
+        if cursor.fetchone() is not None:
+            flash("De ingevoerde email is al in gebruik!")
+            return render_template('register.html')
+        else:
+            cursor.execute("INSERT INTO User (first_name, last_name, email, password, role, company, date_of_birth, account_type, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+                           (first_name, last_name, email, hashed_pw, role, company, birth_date, account_type, created_at))
+            conn.commit()
+            flash("Bedankt voor de registratie. Er kan nu ingelogd worden!")
+            return render_template('register.html')
 
     return render_template('register.html')
 
 # Login
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if 'user_id' in session:
+        return redirect('dashboard')
+    conn = sqlite3.connect("acs.db")
+    cursor = conn.cursor()
+    
+    def hash_password(password):
+        # Hash the password using SHA-256
+        hash_object = hashlib.sha256(password.encode())
+        return hash_object.hexdigest()
+    
     if request.method == 'POST':
-        username = request.form.get('email')
+        email = request.form.get('email')
         password = request.form.get('password')
 
-        user = User(username=username, hashed_password=password)
-        find = user.find()
-        
-        if user.check_password(password.encode('utf8')):
-            session['username'] = username
-            return 'ingelogd'
-    return(render_template('login.html'))
+        hashed_password = hash_password(password)
+
+        # Retrieve the user from the database
+        cursor.execute("SELECT * FROM User WHERE email=? AND password=?", (email, hashed_password))
+        user = cursor.fetchone()
+        if user is not None:
+            session['user_id'] = user[0]
+            return redirect('dashboard')
+        else:
+            flash("Ongeldige gebruikersnaam of wachtwoord.")
+            return render_template('login.html')
+    return render_template('login.html')
 
 
 ##### Dashboard page #####
 # Render dashboard
 @app.route('/dashboard')
 def dashboard():
+    if 'user_id' not in session:
+        return redirect('/')
+    
     mlx_data = MlxData().find("id", 5)
 
     return render_template('dashboard.html', data={"minTemp": mlx_data['min_temp'], "maxTemp": mlx_data['max_temp'], "avgTemp": mlx_data['avg_temp']})
--- server/models.py
+++ server/models.py
@@ -5,59 +5,6 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo, Email
 from datetime import date
 
-class User():
-    def __init__(self, first_name, last_name, username, hashed_password, 
-                 company, date_of_birth, role):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.username = username
-        self.hashed_password = hashed_password
-        self.company = company
-        self.date_of_birth = date_of_birth
-        self.role = role
-        
-    def create(self):
-        conn = sqlite3.connect('acs.db')  # Connect to database
-        cursor = conn.cursor()
-        first_name = self.first_name
-        last_name = self.last_name
-        user = self.username
-        password = self.hashed_password
-        company = self.company
-        date_of_birth = self.date_of_birth
-        role = self.role
-        account_type = 0
-        created_at = date.today()
-
-        # Check if the user exists
-        cursor.execute("SELECT COUNT(*) FROM User WHERE email = ?", (user,))
-        count = cursor.fetchone()[0]
-        if count > 0:
-            return 'Failed to create user, User exists!'
-        else:
-            cursor.execute("INSERT INTO User (first_name, last_name, email, password, role, company, date_of_birth, account_type, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-                           (first_name, last_name, user, password, role, company, date_of_birth, account_type, created_at))
-            conn.commit()
-            return 'User created successfully!'
-        
-    def find(self):
-        conn = sqlite3.connect('acs.db')  # Connect to database
-        cursor = conn.cursor()
-        user = self.username
-
-        # Check if the user exists
-        cursor.execute("SELECT COUNT(*), password FROM User WHERE email = ?", (user,))
-        count = cursor.fetchone()[0]
-        passw = cursor.fetchone()[1]
-        
-        if count > 0:
-            return passw
-        else:
-            return 'User not found or invalid username and password'
-    
-    def check_password(self, other_pw):
-        return User.bcrypt.checkpw(other_pw, self.hashed_password)
-
 class MlxData():
     def __init__(self, id_ = 0, min_temp = 0, max_temp = 0, avg_temp = 0, created_at = ""):
         self.id_ = id_
